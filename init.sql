c7c5669cc61d9f6a70abfb60aa27eb69382c8352
Jack <jack@xfero.xyz>
2019-04-20 23:03:11 +0800

    build(doby): doby

--- init.sql
+++ init.sql
@@ -2,14 +2,12 @@
 create role luxtag_user with login password 'luxtag!23#';
 alter role luxtag_user createdb;
 
-create database transactions;
 create database uuid;
-create database certificates;
 create database users;
 create database tracks;
+create database doby;
 
-GRANT ALL PRIVILEGES ON DATABASE transactions TO luxtag_user;
 GRANT ALL PRIVILEGES ON DATABASE uuid TO luxtag_user;
-GRANT ALL PRIVILEGES ON DATABASE certificates TO luxtag_user;
 GRANT ALL PRIVILEGES ON DATABASE users TO luxtag_user;
 GRANT ALL PRIVILEGES ON DATABASE tracks TO luxtag_user;
+GRANT ALL PRIVILEGES ON DATABASE doby TO luxtag_user;
